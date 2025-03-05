cd8d3aad13bfc166e6c4e38224635aeb54a9b32d
quinn-anaiah1 <aequinn@miners.utep.edu>
2025-03-05 10:24:58 -0700

    user form setup

---
+++ src/config.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyByO5y6WwQBchmOOb8lynhHfLpv3Ie6alI",
+  authDomain: "quinn-sandbox-78f40.firebaseapp.com",
+  projectId: "quinn-sandbox-78f40",
+  storageBucket: "quinn-sandbox-78f40.firebasestorage.app",
+  messagingSenderId: "307838114166",
+  appId: "1:307838114166:web:b0d2f4aa62bc99fd1a4257",
+  measurementId: "G-EJLD1PF04M"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app); //authorization tokeen
+
+export const auth = getAuth(app);
+export const db = getFirestore(app); //get firestore database
+export default app;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const path = require('path')
+//const { auth } = require('./src/config')
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        auth: './src/auth.js',
+        config: './src/config.js'
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: '[name].bundle.js'
     },
     watch: true
 }

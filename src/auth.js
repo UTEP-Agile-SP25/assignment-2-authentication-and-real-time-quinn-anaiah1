335f8cd93c0f9811ccbcaf22c5eb5f53990ed9cf
quinn-anaiah1 <aequinn@miners.utep.edu>
2025-03-07 15:28:16 -0700

    new user created

--- src/auth.js
+++ src/auth.js
@@ -1,6 +1,8 @@
 import { createUserWithEmailAndPassword } from "@firebase/auth";
 import {auth} from "./config";
 import {db} from "./config";
+import {collection, doc, getDoc,deleteDoc,getDocs,setDoc, getFirestore} from "firebase/firestore";
+
 
 export async function signUp(firstName, lastName, email, password){
     try{
@@ -8,12 +10,14 @@ export async function signUp(firstName, lastName, email, password){
         console.log("user signed up: ", userCredential.user.email)
         console.log("User ID: ", userCredential.user.uid)
         const userRef = doc(db, "users", userCredential.user.uid)
+        console.log("Writing to Firestore path: ", userRef.path);
 
         await setDoc(userRef, {
             firstname:firstName,
             lastname: lastName,
             timestamp: new Date()
         })
+    
 
     }catch(error){
         console.error("Error fetching user data ", error);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("index.js");
+import {signUp} from "./auth"
+import {auth} from "./config";
+
+
+
+const signUpForm = document.querySelector("#signupForm")
+signUpForm.addEventListener("submit", (event)=>{
+    event.preventDefault()
+    const firstname = document.getElementById("firstName").value
+    const lastname = document.getElementById("lastName").value
+    const email = document.getElementById("signupEmail").value
+    const password = document.getElementById("signupPassword").value
+
+    signUp(firstname, lastname, email, password)
+    console.log("Current user:", auth.currentUser);
+
+} )
